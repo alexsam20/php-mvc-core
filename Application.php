a66f54203e89771a6a2833958b4256476f5df029
Alex SaM <alsam3637@gmail.com>
2025-03-08 10:43:37 -0500

    Fixed Application.php

--- Application.php
+++ Application.php
@@ -10,44 +10,59 @@ use app\core\db\DbModel;
  */
 class Application
 {
-    public static string $ROOT_DIR;
-    public string $layout = 'main';
-    public string $userClass;
     public static Application $app;
-    public ?Controller $controller = null;
-    public Database $db;
-    public ?UserModel $user;
+    public static string $ROOT_DIR;
+    public string $userClass;
+    public string $layout = 'main';
     public Router $router;
     public Request $request;
     public Response $response;
+    public ?Controller $controller = null;
+    public Database $db;
     public Session $session;
     public View $view;
+    public ?UserModel $user;
 
     public function __construct($rootPath, array $config)
     {
+        $this->user = null;
         self::$ROOT_DIR = $rootPath;
+        $this->userClass = $config['userClass'];
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
         $this->db = new Database($config['db']);
-        $this->userClass = $config['userClass'];
         $this->view = new View();
 
-        $primaryValue = $this->session->get('user');
-        if ($primaryValue) {
+        $userId = Application::$app->session->get('user');
+        if ($userId) {
             $user = new $this->userClass();
-            $primaryKey = $user->primaryKey();
-            $this->user = $user->findOne([$primaryKey => $primaryValue]);
-        } else {
-            $this->user = null;
+            $key = $user->primaryKey();
+            $this->user = $user->findOne([$key => $userId]);
         }
     }
 
     public static function isGuest()
     {
         return !self::$app->user;
+    }
+
+    public function login(UserModel $user): true
+    {
+        $this->user = $user;
+        $primaryKey = $user->primaryKey();
+        $value = $user->{$primaryKey};
+        Application::$app->session->set('user', $value);
+
+        return true;
+    }
+
+    public function logout(): void
+    {
+        $this->user = null;
+        self::$app->session->remove('user');
     }
     public function run(): void
     {
@@ -68,21 +83,6 @@ class Application
     public function setController(Controller $controller): void
     {
         $this->controller = $controller;
-    }
-
-    public function login(UserModel $user): true
-    {
-        $this->user = $user;
-        $primaryKey = $user->primaryKey();
-        $primaryValue = $user->{$primaryKey};
-        $this->session->set('user', $primaryValue);
-        return true;
-    }
-
-    public function logout(): void
-    {
-        $this->user = null;
-        $this->session->remove('user');
     }
 
 }
